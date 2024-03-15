dac57e2b06a4e8a819ee47f15ab042956ae7c262
DmytroKorolyov <148568611+DmytroKorolyov@users.noreply.github.com>
2024-03-15 13:17:13 +0200

    1

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,61 @@
+
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+
+class Person {
+    private key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    private tenants: Person[];
+
+    constructor(key: Key) {
+        this.door = false;
+        this.key = key;
+        this.tenants = [];
+    }
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +65,7 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
-
 export {};
+
+
+
